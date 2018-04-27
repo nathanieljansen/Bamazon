c0123f17bec37fd94cba7f9ecc52fe0bbfec8f94
Nathaniel Jansen <nathanieljansen@gmail.com>
2018-04-26 18:28:20 -0600

    finishing some things up

--- bamazonManager.js
+++ bamazonManager.js
@@ -2,6 +2,8 @@ const mysql = require("mysql")
 
 const inquirer = require("inquirer")
 
+require('events').EventEmitter.prototype._maxListeners = 100;
+
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -31,11 +33,12 @@ function managerPortal() {
         console.log("===================================")
         viewLowInventory();
         break
-      case "Add to Inventory":
+      case "Add a New Product":
         console.log("Add to Inventory")
         console.log("===================================")
+        addProduct();
         break
-      case "Add a New Product":
+      case "Add to Inventory":
         console.log("Add a New Product")
         console.log("===================================")
         break
@@ -51,8 +54,9 @@ function viewProducts(){
     for (var i = 0; i < res.length; i++) {
       items = '';
       items += 'Product Name: ' + res[i].product_name
-      console.log(items);
+      console.log(items.toUpperCase());
     }
+    return setTimeout(function () { managerPortal() }, 4000);
   })
 }
 
@@ -64,18 +68,86 @@ function viewLowInventory() {
       items = '';
       items += 'Product Name: ' + res[i].product_name + '  //  ';
       items += 'Quantity: ' + res[i].stock_quantity + '\n';
-      console.log(items);
+      console.log(items.toUpperCase());
+      
     }
+    return setTimeout(function () { managerPortal() }, 4000);
   })
+}
+
+function addProduct() {
+  inquirer
+    .prompt([
+      {
+        name: "newProduct",
+        type: "input",
+        message: "What is the product you would like to add?"
+      },
+      {
+        name: "department",
+        type: "input",
+        message: "What department would you like to place your product in?"
+      },
+      {
+        name: "price",
+        type: "input",
+        message: "What is the price of this item?",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many do you have?",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      }
+    ])
+    .then(function (answer) {
+      
+      connection.query(
+        "INSERT INTO products SET ?",
+        {
+          product_name: answer.newProduct,
+          department_name: answer.department,
+          price: answer.price,
+          stock_quantity: answer.quantity
+        },
+        function (err) {
+          if (err) throw err;
+          console.log("Your product was created successfully!");
+          setTimeout(function () { }, 3000);
+          inquirer.prompt([{
+            type: 'list',
+            name: 'addMore',
+            message: "Would you like to add another product?",
+            choices: ["Yes", "No"]
+          }]).then(res => {
+            switch (res.addMore) {
+              case "Yes":
+                return addProduct();
+              case "No":
+                console.log("You will be returned to the main screen");
+                return setTimeout(function () { start() }, 2000);
+            }
+          })
+        }
+      );
+    });
 }
 
 function addInventory() {
 
 }
 
-function addProduct() {
-
-}
 
 function start() {
   managerPortal();
